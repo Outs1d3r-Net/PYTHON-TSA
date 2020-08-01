ab66c0bebb1f06c1395d9c44552cc344295cd38a
Outs1d3r-Net <41843160+Outs1d3r-Net@users.noreply.github.com>
2020-08-01 14:47:11 -0300

    Update TSA.py
    Code "#DEPENDENCIES" added !

--- TSA.py
+++ TSA.py
@@ -1,19 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/env python2
 
 #############################
 ### THE SERPENT AWAKENING ###
 #############################
 
 #LIBRARIES
-
 import os
+import sys
 import commands
 
+os.system('clear')
+
+#DEPENDENCIES
+mplayer = os.system('which mplayer')
+if mplayer == 256:
+    c = raw_input('[*] Dependencies [*]\n\nmplayer not installed, for the alarm clock to work properly please install the mplayer program\n[+] Y for install\n[-] N for exit\n>>> ').strip('\n')
+    if c == 'Y' or c == 'y':
+        if os.geteuid() !=0:
+            print 'Error ! required root privilege.'
+            sys.exit(1)
+        os.system('apt install mplayer -y')
+        os.system('clear')
+        print 'End ! Now run the program without root privileges!'
+        sys.exit(0)
+    elif c == 'N' or c == 'n':
+        print 'bye bye...'
+        sys.exit(1)
+    else:
+        print 'Option not allowed !'
+        sys.exit(1)
+
+
 #MAIN
+os.system('clear')
+up = raw_input("Please select data in format: 2020-08-01-07.40.42 (Year-month-day-hour-minutes-seconds)>>> ")
+    #EX: 2020-08-01-07.40.42 HIGHLIGHTER - [YEAR - MONTH - DAY - HOUR - MINUTES - SECONDS]
+    #Please change this field to the date and time of your preference.
+
 while True:
-    up="2018-09-02-18.42.25" #HIGHLIGHTER - [YEAR - MONTH - DAY - HOUR - MINUTES - SECONDS]
-                             #Please change this field to the date and time of your preference.
-        
+
     locks=commands.getoutput("date +%Y-%m-%d-%H.%M.%S") #CLOCK
     if locks == up: #CHECKER
         while True:
